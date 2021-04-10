c034965b3975be2c650769a53d292ca066072d06
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-04-10 10:19:25 -0500

    Initialization
    Added POCO, basic input

---
+++ EmailAddressMap/EmailAddressMap/Map.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailAddressMap
+{
+    public class Map
+    {
+        private string[] Key { get; set; }
+        private string[] Value { get; set; }
+        private int Max { get; set; }
+
+        public Map() {
+        
+        }
+
+        public Map(string[] key, string[] value, int max)
+        {
+            Value = value;
+            Max = max;
+
+            //Populate the Key array with hashed keys
+            for(int i = 0; i < key.Length; i++)
+            {
+                Key[i] = Hash(key[i], Max).ToString();
+            }
+        }
+
+        public static int Hash(string key, int max)
+        {
+            int h = 0;
+            for (int i = 0; i < key.Length; i++)
+            {
+                h += key[i] * (max - 1);
+            }
+            return h % max;
+        }
+    }
+}
---
+++ EmailAddressMap/EmailAddressMap/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmailAddressMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] emails = { "example@mail.com" };
+            string[] names = { "Marjorie Smith" };
+            Map m = new Map(emails, names, 5);
+        }
+    }
+}
