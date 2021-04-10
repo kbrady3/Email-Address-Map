d7ce77ecd886729ca057cb7b14e6256d59e0631d
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-04-10 11:53:34 -0500

    Completed

--- EmailAddressMap/EmailAddressMap/Map.cs
+++ EmailAddressMap/EmailAddressMap/Map.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//This map uses 3 primary lists: NonHashedKeys, Key, and Value. NonHashedKeys stores sensitive information and is not directly accessible to the user. This list is necessary for finding values that already exist, because users won't be searching by hashed keys, they'll be searching by the unencrypted keys.
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,8 +8,10 @@ namespace EmailAddressMap
 {
     public class Map
     {
-        private List<string> Key { get; set; }
+        private List<string> NonHashedKeys { get; set; }
+        private List<int> Key { get; set; }
         private List<string> Value { get; set; }
+        private List<int> EmptyList = new List<int>() { 0 }; //Used to initialize Key so it won't throw a null error
         private int Max { get; set; }
 
         public Map() {
@@ -16,20 +20,24 @@ namespace EmailAddressMap
 
         public Map(List<string> key, List<string> value, int max)
         {
-            if(key.Count != value.Count)
+            Key = EmptyList; //Initialize Key so it won't throw a null error
+
+            if (key.Count != value.Count)
             {
                 throw new ArgumentException("Key and value lists must be the same length.");
             }
 
+            NonHashedKeys = key;
             Value = value;
-            Key = key; //Initialize array with provided keys before sending to Hash() method
             Max = max;
 
             //Populate the Key array with hashed keys
             for (int i = 0; i < key.Count; i++)
             {
-                Key[i] = Hash(key[i], Max).ToString();
+                Key.Add(Hash(key[i], Max));
             }
+
+            Key.RemoveAt(0);
         }
 
         public static int Hash(string key, int max)
@@ -40,6 +48,77 @@ namespace EmailAddressMap
                 h += key[i] * (max - 1);
             }
             return h % max;
+        }
+
+        public void Insert(string k, string v)
+        {
+            if (NonHashedKeys.Contains(k))
+            {
+                throw new ArgumentException("Key already exists.");
+            }
+            else
+            {
+                NonHashedKeys.Add(k);
+                int keyHashed = Hash(k, Max);
+                Key.Add(keyHashed);
+                Value.Add(v);
+            }
+        }
+
+        public void Remove(string k)
+        {
+            if (!NonHashedKeys.Contains(k))
+            {
+                throw new ArgumentException("Key doesn't exist.");
+            }
+            else
+            {
+                int keyIndex = NonHashedKeys.IndexOf(k);
+                NonHashedKeys.RemoveAt(keyIndex);
+                Key.RemoveAt(keyIndex);
+                Value.RemoveAt(keyIndex);
+            }
+        }
+
+        public void RemoveAll()
+        {
+            Key.Clear();
+            Value.Clear();
+            NonHashedKeys.Clear();
+        }
+
+        public bool HasKey(string valueToSearch)
+        {
+            if (Value.Contains(valueToSearch))
+            {
+                int keyIndex = Value.IndexOf(valueToSearch); //Since Key/Value are parallel arrays, gets the index of provided value and then gets the index of the corresponding key
+
+                if (Key[keyIndex] != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("The given value doesn't exist.");
+            }
+        }
+
+        public string FindValue(string keyToSearch)
+        {
+            if (NonHashedKeys.Contains(keyToSearch))
+            {
+                int valueIndex = NonHashedKeys.IndexOf(keyToSearch);
+                return Value[valueIndex];
+            }
+            else
+            {
+                throw new ArgumentException("The given key doesn't exist.");
+            }
         }
 
         public string Print()
--- EmailAddressMap/EmailAddressMap/Program.cs
+++ EmailAddressMap/EmailAddressMap/Program.cs
@@ -7,9 +7,14 @@ namespace EmailAddressMap
     {
         static void Main(string[] args)
         {
-            //Instead of using multiple Add methods to get all the values into the lists, I created arrays and used for loops to populate the lists. In my opinion, simply adding to an array by typing in the value is easier than using List.Add().
+            //Create empty map
+            List<string> emptyKey = new List<string>();
+            List<string> emptyValue = new List<string>();
+            Map emptyMap = new Map(emptyKey, emptyValue, 0);
 
-            string[] emailsArray = { "example@mail.com", "test@mail.com", "me@mail.com", "you@mail.com" };
+
+            //Instead of using multiple Add methods to get all the values into the lists, I created arrays and used for loops to populate the lists. In my opinion, simply adding to an array by typing in the values is easier than using List.Add().
+            string[] emailsArray = { "example@mail.com", "test@mail.com", "", "you@mail.com" };
             string[] namesArray = { "Marjorie Smith", "Bailey Estra", "Sam Smiley", "Bob Robert" };
             List<string> emails = new List<string>();
             List<string> names = new List<string>();
@@ -27,6 +32,21 @@ namespace EmailAddressMap
             Map m = new Map(emails, names, 78542);
 
             Console.WriteLine(m.Print());
+            Console.WriteLine("Sam Smiley has an associated key: " + m.HasKey("Sam Smiley") + "\n");
+            //Search for 2 people by key
+            Console.WriteLine("Find value associated with 'test@mail.com': " + m.FindValue("test@mail.com") + "\n");
+            Console.WriteLine("Find value associated with 'you@mail.com': " + m.FindValue("you@mail.com") + "\n");
+            //Add 3 people
+            m.Insert("i@example.com", "George Larrius");
+            m.Insert("tu@mail.com", "Sally Lu");
+            m.Insert("mi@mail.com", "Alex Shu");
+            Console.WriteLine("New items added, new list below: \n" + m.Print() + "\n");
+            m.Remove("mi@mail.com");
+            Console.WriteLine("Previously added item removed, new list below: \n" + m.Print() + "\n");
+
+            m.RemoveAll();
+
+            Console.WriteLine("All values removed. List is now empty, as proven here (nothing should show up): " + m.Print());
         }
     }
 }
